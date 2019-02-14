1037148637eee5b3d1b57b688a488d646f7adabc
Putra _ <n1ghtpe0ple@protonmail.com>
2019-02-14 11:08:44 +0700

    Update linker.sh

--- linker.sh
+++ linker.sh
@@ -7,7 +7,7 @@ lightgreen='\e[1;32m'
 white='\e[1;37m'
 red='\e[1;31m'
 yellow='\e[1;33m'
-bin='$PREFIX/bin/'
+bin='$PREFIX/bin/termux-elf-cleaner'
 echo -e $okegreen" [ * ] Updating Termux ..."
 pkg update
 echo -e $cyan" [ * ] Done !!"
