ba7c103cfef83832a1401bca796618f2f6c2e8c9
Putra _ <n1ghtpe0ple@protonmail.com>
2019-02-17 09:46:48 +0700

    Update linker.sh

--- linker.sh
+++ linker.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 blue='\e[0;34'
 cyan='\e[0;36m'
 green='\e[0;34m'
@@ -7,35 +8,36 @@ lightgreen='\e[1;32m'
 white='\e[1;37m'
 red='\e[1;31m'
 yellow='\e[1;33m'
-echo -e $okegreen" [ * ] Updating Termux ..."
+
+echo -e "$cyan [$okegreen *$cyan ]$white Updating Termux ..."
 pkg update
-echo -e $cyan" [ * ] Done !!"
-echo -e $okegreen" [ * ] Installing Dependencies ..."
-echo -e $okegreen" [ - ] Installing Make ..."
+echo -e "$cyan [$okegreen *$cyan ]$okegreen Done !!"
+echo -e "$cyan [$okegreen *$cyan ]$white Installing Dependencies ..."
+echo -e "$cyan [$okegreen *$cyan ]$white Installing Make ..."
 pkg install make -y
-echo -e $okegreen" [ - ] Installing Clang ..."
+echo -e "$cyan [$okegreen *$cyan ]$white Installing Clang ..."
 pkg install clang -y
-echo -e $okegreen" [ - ] Installing Git ..."
+echo -e "$cyan [$okegreen *$cyan ]$white Installing Git ..."
 pkg install git -y
-echo -e $cyan" [ * ] Done !!"
-echo -e $okegreen" [ * ] Getting termux-elf-cleaner Script ..."
+echo -e "$cyan [$okegreen *$cyan ]$okegreen Done !!"
+echo -e "$cyan [$okegreen *$cyan ]$white Getting termux-elf-cleaner Script ..."
 git clone https://github.com/Harshiv-Patel/termux-elf-cleaner.git
 cd termux-elf-cleaner
-echo -e $cyan" [ * ] Done !!"
-echo -e $okegreen" [ * ] Building Script ..."
+echo -e "$cyan [$okegreen *$cyan ]$okegreen Done !!"
+echo -e "$cyan [$okegreen *$cyan ]$white Building Script ..."
 make
-echo -e $cyan" [ * ] Done !!"
-echo -e $okegreen" [ * ] Copying Script to $PREFIX/bin/ ..."
+echo -e "$cyan [$okegreen *$cyan ]$okegreen Done !!"
+echo -e "$cyan [$okegreen *$cyan ]$white Copying Script to $PREFIX/bin/ ..."
 cp termux-elf-cleaner $PREFIX/bin/
-echo -e $cyan" [ * ] Done !!"
-echo -e $okegreen" [ * ] Fixing Termux Linker ..."
+echo -e "$cyan [$okegreen *$cyan ]$okegreen Done !!"
+echo -e "$cyan [$okegreen *$cyan ]$white Fixing Termux Linker ..."
 termux-elf-cleaner /data/data/com.termux/files/usr/lib/libcrypto.so
 termux-elf-cleaner /data/data/com.termux/files/usr/lib/libssl.so
-echo -e $cyan" [ * ] Done !!"
-echo -e $okegreen" [ * ] Cleaning Junk ..."
+echo -e "$cyan [$okegreen *$cyan ]$okegreen Done !!"
+echo -e "$cyan [$okegreen *$cyan ]$white Cleaning Junk ..."
 cd ..
 rm -rf termux-elf-cleaner
-echo -e $cyan" [ * ] Done !!"
+echo -e "$cyan [$okegreen *$cyan ]$okegreen Done !!"
 echo ""
-echo -e $cyan" [ * ] ALL Done !!"
+echo -e "$cyan [$okegreen *$cyan ]$okegreen Done !!"
 echo -e $white""
